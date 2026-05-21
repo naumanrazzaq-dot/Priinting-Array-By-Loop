781c0d30295a58123e39413beceb4fbec91ac8d4
Muhammad Nauman <maliknomigep@gmail.com>
2026-05-21 21:40:43 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    for(int i=0;i<=4;i++){
+        cin>>arr[i];
+    }
+        for(int j=0; j<=4;j++){
+            cout<<arr[j];
+            cout<<endl;
+
+    }
+    return 0;
+}
